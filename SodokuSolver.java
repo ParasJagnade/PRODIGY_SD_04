50bd826a14859afa607af27d281f75e3552cdcea
ParasJagnade <jagnadeparas0602@gmail.com>
2026-01-14 20:18:17 +0530

    Update SodokuSolver.java

--- SodokuSolver.java
+++ SodokuSolver.java
@@ -1,25 +1,24 @@
 public class SudokuSolver {
 
-    static final int SIZE = 9;
-
-    public static void main(String[] args) {
-
-        int[][] board = {
+    static int[][] board = {
             {5, 3, 0, 0, 7, 0, 0, 0, 0},
             {6, 0, 0, 1, 9, 5, 0, 0, 0},
             {0, 9, 8, 0, 0, 0, 0, 6, 0},
+
             {8, 0, 0, 0, 6, 0, 0, 0, 3},
             {4, 0, 0, 8, 0, 3, 0, 0, 1},
             {7, 0, 0, 0, 2, 0, 0, 0, 6},
+
             {0, 6, 0, 0, 0, 0, 2, 8, 0},
             {0, 0, 0, 4, 1, 9, 0, 0, 5},
             {0, 0, 0, 0, 8, 0, 0, 7, 9}
-        };
+    };
 
+    public static void main(String[] args) {
         System.out.println("Unsolved Sudoku:");
         printBoard(board);
 
-        if (solveSudoku(board)) {
+        if (solve(board)) {
             System.out.println("\nSolved Sudoku:");
             printBoard(board);
         } else {
@@ -27,16 +26,16 @@ public class SudokuSolver {
         }
     }
 
-    static boolean solveSudoku(int[][] board) {
-        for (int row = 0; row < SIZE; row++) {
-            for (int col = 0; col < SIZE; col++) {
-
+    static boolean solve(int[][] board) {
+        for (int row = 0; row < 9; row++) {
+            for (int col = 0; col < 9; col++) {
                 if (board[row][col] == 0) {
+
                     for (int num = 1; num <= 9; num++) {
-                        if (isSafe(board, row, col, num)) {
+                        if (isValid(board, row, col, num)) {
                             board[row][col] = num;
 
-                            if (solveSudoku(board))
+                            if (solve(board))
                                 return true;
 
                             board[row][col] = 0;
@@ -49,27 +48,38 @@ public class SudokuSolver {
         return true;
     }
 
-    static boolean isSafe(int[][] board, int row, int col, int num) {
-        for (int x = 0; x < SIZE; x++) {
-            if (board[row][x] == num || board[x][col] == num)
+    static boolean isValid(int[][] board, int row, int col, int num) {
+
+        // Check row
+        for (int x = 0; x < 9; x++) {
+            if (board[row][x] == num)
                 return false;
         }
 
+        // Check column
+        for (int x = 0; x < 9; x++) {
+            if (board[x][col] == num)
+                return false;
+        }
+
+        // Check 3x3 grid
         int startRow = row - row % 3;
         int startCol = col - col % 3;
 
-        for (int i = 0; i < 3; i++)
-            for (int j = 0; j < 3; j++)
-                if (board[i + startRow][j + startCol] == num)
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (board[startRow + i][startCol + j] == num)
                     return false;
+            }
+        }
 
         return true;
     }
 
     static void printBoard(int[][] board) {
-        for (int r = 0; r < SIZE; r++) {
-            for (int d = 0; d < SIZE; d++) {
-                System.out.print(board[r][d] + " ");
+        for (int row = 0; row < 9; row++) {
+            for (int col = 0; col < 9; col++) {
+                System.out.print(board[row][col] + " ");
             }
             System.out.println();
         }
